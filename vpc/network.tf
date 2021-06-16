3c57e30578e1ebec9c665e5387ef887f886835bd
stapie <ghostbossman@gmail.com>
2021-06-16 15:24:23 +0300

    ec2 module

--- vpc/network.tf
+++ vpc/network.tf
@@ -1,10 +1,8 @@
 
 terraform {
-  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
+
   required_version = "= 1.0.0"
 
-  # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -6,3 +6,11 @@ output "security_group_id" {
 output "jenkins_vpc_id" {
   value = aws_vpc.jenkins-vpc.id
 }
+
+output "subnet_id" {
+  value = aws_subnet.subnet_1_jen.id
+}
+
+output "aws_main_route_table_association_id"{
+  value = aws_main_route_table_association.set-master-default-rt-assoc_jen.id
+}
