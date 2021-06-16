466be20102fc92f9e42fe9b0fc909cc22afe9a8d
stapie <ghostbossman@gmail.com>
2021-06-16 15:55:30 +0300

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "jenkins" {
   provisioner "local-exec" {
     command = <<EOF
 aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region} --instance-ids ${self.id}
-ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ../playbook.yaml
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' playbook.yaml
 EOF
   }
 
